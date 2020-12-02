60c6bab7f120882a9f445439877d787864d4aeff
= <=>
2020-12-02 17:06:25 +0300

    code defactoring

--- Sudoku_Genarator.py
+++ Sudoku_Genarator.py
@@ -2,41 +2,41 @@ import random
 import copy
 
 
-def format(sudoku):
-    sudoku_formatted = sudoku
-    for row in range(len(sudoku_formatted)):
+def format(grid):
+    grid_formatted = copy.deepcopy(grid)
+    for row in range(len(grid_formatted)):
         for i in [3, 7]:
-            sudoku_formatted[row].insert(i, '|')
+            grid_formatted[row].insert(i, '|')
 
         if row % 3 == 0 and row != 0:
             print('- - - - - - - - - - - - - - - - - - ')
-        print(sudoku_formatted[row])
+        print(grid_formatted[row])
 
 
-def is_grid_full(sudoku_grid):
+def is_grid_full(grid_grid):
     for row in range(9):
         for column in range(9):
-            if sudoku_grid[row][column] == 0:
+            if grid_grid[row][column] == 0:
                 return False
     return True
 
 
-def find_empty_square(sudoku):
-    for row in range(len(sudoku)):
-        for column in range(len(sudoku[row])):
-            if sudoku[row][column] == 0:
+def find_empty_square(grid):
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 0:
                 return row, column
 
 
-def check_square(number, row_index, column_index, sudoku):
+def check_square(number, row_index, column_index, grid):
     checked_number = number
     # check on row
-    for number in sudoku[row_index]:
+    for number in grid[row_index]:
         if number == checked_number:
             return False
 
     # check on column
-    for row in sudoku:
+    for row in grid:
         if row[column_index] == checked_number:
             return False
 
@@ -45,29 +45,29 @@ def check_square(number, row_index, column_index, sudoku):
     square_column = round(column_index/3-0.3)
     for row in range(square_row*3, square_row*3+3):
         for column in range(square_column*3, square_column*3+3):
-            if sudoku[row][column] == checked_number:
+            if grid[row][column] == checked_number:
                 return False
     return True
 
 
-def solve_sudoku(sudoku):
+def solve_sudoku(grid):
     global counter
-    find = find_empty_square(sudoku)
+    find = find_empty_square(grid)
     if not find:
         return True
     row_index, column_index = find
     for i in range(1, 10):
-        if check_square(i, row_index, column_index, sudoku):
-            sudoku[row_index][column_index] = i
+        if check_square(i, row_index, column_index, grid):
+            grid[row_index][column_index] = i
 
             if is_grid_full(grid):
                 counter += 1
                 break
             else:
-                if solve_sudoku(sudoku):
+                if solve_sudoku(grid):
                     return True
 
-            sudoku[row_index][column_index] = 0
+            grid[row_index][column_index] = 0
 
     return False
 
@@ -75,7 +75,7 @@ def solve_sudoku(sudoku):
 def create_empty_grid():
     empty_grid = []
     for x in range(9):
-        empty_grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        empty_grid.append([0,0,0,0,0,0,0,0,0])
     return empty_grid
 
 
@@ -96,9 +96,6 @@ def fill_grid(grid):
             grid[row][column] = 0
     return False
 
-    if not is_grid_full(grid):
-        solve_sudoku(grid)
-    return True
 
 
 def remove_numbers_from_grid(grid):
@@ -116,14 +113,18 @@ def remove_numbers_from_grid(grid):
 
             counter = 0
             solve_sudoku(grid_copy)
+            print(counter)
             if counter != 1:
                 grid[row][column] = deleted_number
-                attempts -= 1
+                break
 
 
-grid = create_empty_grid()
-fill_grid(grid)
+def generate_sudoku():
+    grid = create_empty_grid()
+    fill_grid(grid)
+    remove_numbers_from_grid(grid)
+    format(grid)
+    return grid
 
-remove_numbers_from_grid(grid)
-solve_sudoku(grid)
+#grid = generate_sudoku()
 
